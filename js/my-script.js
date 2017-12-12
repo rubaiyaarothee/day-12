6654b3235ca39329f83983739783eb06402c1405
rubaiyaarothee <rubaiyaarothee@gmail.com>
2017-12-12 13:11:02 +0600

    new file

---
+++ js/my-script.js
@@ -0,0 +1,72 @@
+$('#btn').click(function () {
+  var  inputOneValue = $('#inputOne').val();
+    var  inputTwoValue = $('#inputTwo').val();
+    var result = inputOne + inputTwo;
+ $('#result').val(result);
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+$('#h1').text('hello world');
+
+// var redBtnElement = document.getElementById('redBtn');
+// redBtnElement.onmouseover = function () {
+//   var divOneElement = document.getElementById('divOne');
+//   divOneElement.style.backgroundColor = 'red';
+//   divOneElement.className = 'class-one';
+//
+//
+// };
+// var greenBtnElement = document.getElementById('greenBtn');
+// greenBtnElement.onmouseover = function () {
+//     var divOneElement = document.getElementById('divOne')
+//     divOneElement.style.backgroundColor = 'green';
+//     divOneElement.className = 'class-two';
+// };
+// var blueBtnElement = document.getElementById('blueBtn');
+// blueBtnElement.onmouseover = function () {
+//     var divOneElement = document.getElementById('divOne')
+//     divOneElement.style.backgroundColor = 'blue';
+//     divOneElement.className = 'class-three';
+// };
+// var defaultBtnElement = document.getElementById('defaultBtn');
+// defaultBtnElement.onmouseover = function () {
+//     var divOneElement = document.getElementById('divOne')
+//     divOneElement.style.backgroundColor = 'white';
+// };
+//
+//
+//
+//
+//
+//
+// var btnElement = document.getElementById('btn');
+// btnElement.onclick = function () {
+//
+// var startingNumberValue = document.getElementById('startingNumber').value;
+//     var endingNumberValue = document.getElementById('endingNumber').value;
+//     var res = '';
+//
+//     for (var x=startingNumberValue; x<=endingNumberValue; x++){
+//       res+=x; //res(string) = res + x(string)
+//     }
+// document.getElementById('result').value = res;
+//
+// };
